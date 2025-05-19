835c96ea582646d2437bc4c20044a7b770251c1b
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-05-19 22:43:56 +0300

    задача 4.4.5 вторая проверка

--- src/Deserializer.java
+++ src/Deserializer.java
@@ -1,30 +1,21 @@
 import java.io.*;
 
 public class Deserializer {
-    public static Animal[] deserializeAnimalArray(byte[] data) throws ClassNotFoundException {
+    public static Animal[] deserializeAnimalArray(byte[] data) throws ClassNotFoundException, IOException {
         try (ByteArrayInputStream byteArray = new ByteArrayInputStream(data);
-             ObjectInputStream ois = new ObjectInputStream(byteArray)
-        ) {
+             ObjectInputStream ois = new ObjectInputStream(byteArray)) {
             int arraySize = ois.readInt();
-
-            if (arraySize <= 0 || arraySize > Integer.MAX_VALUE) {
-                throw new IllegalArgumentException("Недопустимый размер массив");
-            }
 
             Animal[] result = new Animal[arraySize];
 
             for (int i = 0; i < arraySize; i++) {
                 Object animal = ois.readObject();
 
-                if (!(animal instanceof Animal)) {
-                    throw new IllegalArgumentException("Не экземпляр Animal");
-                }
-
                 result[i] = (Animal) animal;
             }
 
             return result;
-        } catch (IOException | RuntimeException e) {
+        } catch (NegativeArraySizeException | EOFException | ClassCastException e) {
             throw new IllegalArgumentException("Недопустимые данные для десериализации");
         }
 
