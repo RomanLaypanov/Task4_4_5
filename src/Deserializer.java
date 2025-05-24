7b8c4e7700feb6fd2513f353e7014add19131140
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-05-25 02:52:51 +0300

    задача 4.4.5 пятая проверка

--- src/Deserializer.java
+++ src/Deserializer.java
@@ -2,20 +2,19 @@ import java.io.*;
 
 public class Deserializer {
     public static Animal[] deserializeAnimalArray(byte[] data) throws ClassNotFoundException, IOException {
-        try (ByteArrayInputStream byteArray = new ByteArrayInputStream(data);
-             ObjectInputStream ois = new ObjectInputStream(byteArray)) {
+        try (ByteArrayInputStream byteArray = new ByteArrayInputStream(data); ObjectInputStream ois = new ObjectInputStream(byteArray)) {
             int arraySize = ois.readInt();
 
             Animal[] result = new Animal[arraySize];
 
             for (int i = 0; i < arraySize; i++) {
                 Object animal = ois.readObject();
-
                 result[i] = (Animal) animal;
             }
 
             return result;
-        } catch (ArrayIndexOutOfBoundsException | EOFException | ClassCastException | ArrayStoreException e) {
+        } catch (ArrayIndexOutOfBoundsException | ClassCastException | EOFException | NegativeArraySizeException |
+                 NullPointerException e) {
             throw new IllegalArgumentException("Недопустимые данные для десериализации");
         }
 
